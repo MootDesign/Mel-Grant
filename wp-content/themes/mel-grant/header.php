38a4491f987bef7a627ba6dfc6db08b752c4a57b
Dean Richards <mootdean@gmail.com>
2017-02-03 17:32:50 +0000

    site changes

--- wp-content/themes/mel-grant/header.php
+++ wp-content/themes/mel-grant/header.php
@@ -34,3 +34,26 @@
 <div class="container-full-width">
 <img class="banner-image" src="<?php echo get_template_directory_uri(); ?>/img/banner.jpg">
 </div>
+
+	
+<script type="text/javascript">
+	/* Open when someone clicks on the span element */
+function openNav() {
+	jQuery(document).ready(function($){
+   	 	document.getElementById("myNav").style.width = "100%";
+   	 	$('.bar-one').addClass("open");
+    	$('.bar-two').addClass("open");
+    	$('.bar-three').addClass("open");
+    });
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+	jQuery(document).ready(function($){
+    	$('.bar-one').removeClass("open");
+    	$('.bar-two').removeClass("open");
+    	$('.bar-three').removeClass("open");
+    	$('.toggle-menu').removeClass("open");
+    });
+}
+</script>
