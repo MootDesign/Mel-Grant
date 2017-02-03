3b7ae5efdc80507391448c833235af540ff219bc
Dean Richards <mootdean@gmail.com>
2017-02-03 17:11:28 +0000

    site changes

--- wp-content/themes/mel-grant/header.php
+++ wp-content/themes/mel-grant/header.php
@@ -23,8 +23,8 @@
 	?>
 	
 <body <?php body_class( $awesome_classes ); ?>>
-<div class="navbar-header">
-<button style="display: block!important;" onclick="openNav()" type="button" class="navbar-toggle collapsed full-height-menu-toggle toggle-menu" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+<div class="navbar-header" style="display: block!important; position: absolute; top: 5vw; left: 5vw;">
+<button onclick="openNav()" type="button" class="navbar-toggle collapsed full-height-menu-toggle toggle-menu" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 <span class="st0-menu bar-one"></span>
 <span class="st0-menu bar-two"></span>
 <span class="st0-menu bar-three"></span>
