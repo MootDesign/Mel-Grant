de9d24aefe29986479137087d4985698748289fb
Dean Richards <mootdean@gmail.com>
2017-02-03 17:02:13 +0000

    site changes

--- wp-content/themes/mel-grant/header.php
+++ wp-content/themes/mel-grant/header.php
@@ -23,28 +23,6 @@
 	?>
 	
 <body <?php body_class( $awesome_classes ); ?>>
-
-<header>
-        <nav id="mainNav" class="navbar navbar-custom navbar-fixed-top affix-top">
-          <!-- Original nav <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top affix-top"> -->
-          <div class="container">
-              <!-- Brand and toggle get grouped for better mobile display -->
-              <div class="navbar-header page-scroll">
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                      <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                  </button>
-                  <a class="navbar-brand page-scroll [ white ]" href="#page-top"><i class="fa fa-bars" aria-hidden="true"></i> Menu</a>
-              </div>
-
-              <!-- Collect the nav links, forms, and other content for toggling -->
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                  
-              </div>
-              <!-- /.navbar-collapse -->
-          </div>
-          <!-- /.container-fluid -->
-        </nav>
-</header>
 <div class="navbar-header" align="center">
 					      <button onclick="openNav()" type="button" class="navbar-toggle collapsed full-height-menu-toggle toggle-menu" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 										<span class="st0-menu bar-one"></span>
