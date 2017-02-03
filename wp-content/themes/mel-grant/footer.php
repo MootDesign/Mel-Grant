fb1a95c4cb403818308418229ee03b9c33170542
Dean Richards <mootdean@gmail.com>
2017-02-03 17:30:46 +0000

    site changes

--- wp-content/themes/mel-grant/footer.php
+++ wp-content/themes/mel-grant/footer.php
@@ -1,4 +1,25 @@
 	<?php wp_footer(); ?>
 	
+<script type="text/javascript">
+	/* Open when someone clicks on the span element */
+function openNav() {
+	jQuery(document).ready(function($){
+   	 	document.getElementById("myNav").style.width = "100%";
+   	 	$('.bar-one').addClass("open");
+    	$('.bar-two').addClass("open");
+    	$('.bar-three').addClass("open");
+    });
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+	jQuery(document).ready(function($){
+    	$('.bar-one').removeClass("open");
+    	$('.bar-two').removeClass("open");
+    	$('.bar-three').removeClass("open");
+    	$('.toggle-menu').removeClass("open");
+    });
+}
+</script>
 	</body>
 </html>
