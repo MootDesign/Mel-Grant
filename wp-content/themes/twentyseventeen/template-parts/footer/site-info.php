1d0be1cc7612ddd405479dbe560e6cf5511d33f2
Dean Richards <mootdean@gmail.com>
2017-02-07 10:19:24 +0000

    removed footer

--- wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
+++ wp-content/themes/twentyseventeen/template-parts/footer/site-info.php
@@ -9,6 +9,6 @@
  */
 
 ?>
-<div class="site-info">
+<!--<div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
-</div><!-- .site-info -->
+</div>site-info -->
